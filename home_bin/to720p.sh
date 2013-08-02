0292a7366f11d6b82c8b660f9a9d2b66dd0ef3d3
Sandip Bhattacharya <sandipb@yahoo-inc.com>
2013-08-03 00:14:39 +0530

    tune script

--- home_bin/to720p.sh
+++ home_bin/to720p.sh
@@ -3,16 +3,14 @@ SRC=${1:?Need source video}
 DST=${2:?Need dest video}
 shift
 shift
-avconv -ss 0 -i "$SRC" $* -c:v libx264 \
-                 -c:a aac \
+/usr/bin/ffmpeg -ss 0 -i "$SRC" $* -c:v libx264 -c:a libmp3lame \
                  -strict experimental \
                  -preset medium \
                  -crf 20 \
-                 -s:v hd720 \
-                 -b 5000k \
-                 -ab 320k \
-                 -flags +cgop \
-                 -threads 0 \
+                 -s:v hd720 -maxrate 7000k -minrate 3000k \
+                 -bufsize 1835k \
+                 -b:a 192k \
+                 -flags +cgop -threads 0 \
                  "$DST"
 
 
